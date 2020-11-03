4702fa946709b507bd84bc296dcdcdeb39900b8d
steampower33 <steampower33@naver.com>
2020-11-03 18:54:18 +0900

    변경사항

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Argorithm_Solve
+# Algorithm_Solve
  
--- python_solve/test.py
+++ python_solve/test.py
@@ -1,10 +1,1 @@
-a = dict()
-
-for i in range(1,6): a[i] = 0
-
-print(a)
-
-def dictReset(dict):
-    for i in range(1, len(dict)): dict[i] = -1
-
-print(a)
+print("aa")
