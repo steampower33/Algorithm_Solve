be164bfe2796e37ca2a6fde68c9a42ac0e0bd392
steampower33 <steampower33@naver.com>
2020-11-12 03:52:52 +0900

    해결

--- python_solve/Backtracking/15650 N과 M 2.py
+++ python_solve/Backtracking/15650 N과 M 2.py
@@ -1,20 +1,17 @@
-import sys
-
-def dfs(depth, n, m):
+def dfs(depth, last, n, m):
     if depth == m:
         print(' '.join(map(str, result)))
         return
-    for i in range(len(visited)):
+    for i in range(last, len(visited)):
         if not visited[i]:
             visited[i] = True
             result.append(i + 1)
-            dfs(depth + 1, n, m)
+            dfs(depth + 1, i + 1, n, m)
             visited[i] = False
             result.pop()
 
 if __name__ == "__main__":
-    n, m = map(int, sys.stdin.readline().rsplit())
+    n, m = map(int, input().split())
     visited = [False] * n
     result = list()
-    dfs(0, n, m)
-
+    dfs(0, 0, n, m)
