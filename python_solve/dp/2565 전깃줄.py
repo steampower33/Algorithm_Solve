4246d11c2626ab884cf5d2fbfaf67ba5a686f90e
steampower33 <steampower33@naver.com>
2020-11-16 23:55:05 +0900

    해결

--- python_solve/dp/2565 전깃줄.py
+++ python_solve/dp/2565 전깃줄.py
@@ -3,14 +3,14 @@ import sys
 if __name__ == "__main__":
     n = int(input())
     wires = list()
+    dp = [1]*n
     for i in range(n):
-        wires.append(list(map(int,sys.stdin.readline().rsplit())))
+        wires.append(list(map(int, sys.stdin.readline().rsplit())))
 
-    crossWiresCnt = dict()
-    for i in range(1, n+1):
-        crossWiresCnt[i] = 0
-
+    wires.sort(key= lambda x: x[0])
     for i in range(n):
-        for j in range(n):
-            if i == j: continue
-            if
+        for j in range(i):
+            if wires[i][1] > wires[j][1]:
+                dp[i] = max(dp[i], dp[j]+1)
+
+    print(n-max(dp))
