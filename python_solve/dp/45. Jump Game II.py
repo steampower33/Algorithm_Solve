fff6597e29f16f9b3c9ba11b4a15a3dde3e0e426
steampower33 <steampower33@naver.com>
2022-04-19 15:35:29 +0900

    leetcode 45. Jump Game II solve:python3

---
+++ python_solve/dp/45. Jump Game II.py
@@ -0,0 +1,15 @@
+def jump(nums):
+    end, cnt, max_num = 0, 0, 0;
+    for _ in range(len(nums)):
+        max_num = max(max_num, _+nums[_])
+
+        if len(nums) == 1: return 0
+
+        if _ == end:
+            end = max_num
+            cnt += 1
+
+        if len(nums)-1 <= end:
+            return cnt
+
+print(jump([0]))
