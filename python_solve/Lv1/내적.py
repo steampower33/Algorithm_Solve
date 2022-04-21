f565a0ddce8579c585146423c35cdfaa99c0eebb
steampower33 <steampower33@naver.com>
2022-04-21 18:19:50 +0900

    프로그래머스 Lv1. 내적 solve:python3

---
+++ python_solve/Lv1/내적.py
@@ -0,0 +1,14 @@
+def solution(a, b):
+    answer =0
+
+    for a, b in zip(a, b):
+        answer += a * b
+
+    return answer
+
+'''
+
+def solution(a, b):
+
+    return sum([x*y for x, y in zip(a,b)])
+'''
