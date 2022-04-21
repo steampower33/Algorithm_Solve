3694976d2fe882824cce6b561dd015684cc8a932
steampower33 <steampower33@naver.com>
2022-04-21 17:09:09 +0900

    프로그래머스 Lv1. 키패드 누르기 수정

--- python_solve/Lv1/키패드 누르기.py
+++ python_solve/Lv1/키패드 누르기.py
@@ -3,44 +3,38 @@ import math
 def solution(numbers, hand):
     answer = ''
 
-    keypad = {1: (1, 1), 2: (1, 2), 3: (1, 3), 4: (2, 1), 5: (2, 2), 6: (2, 3), 7: (3, 1), 8: (3, 2), 9: (3, 3), 0: (4, 2)}
+    keypad = {1: (0,0), 2: (0,1), 3: (0,2),
+              4: (1,0), 5: (1,1), 6: (1,2),
+              7: (2,0), 8: (2,1), 9: (2,2),
+              '*': (3,0), 0: (3,1), '#': (3,2)}
 
-    dist_l = (4,1)
-    dist_r = (4,3)
+    lhand = keypad['*']
+    rhand = keypad['#']
 
     for num in numbers:
         if num in [1, 4, 7]:
             answer += 'L'
-            dist_l = keypad[num]
+            lhand = keypad[num]
         elif num in [3, 6, 9]:
             answer += 'R'
-            dist_r = keypad[num]
+            rhand = keypad[num]
         else:
-            d_l = math.sqrt((keypad[num][0] - dist_l[0]) ** 2 + abs(keypad[num][1] - dist_l[1]) ** 2)
-            d_r = math.sqrt((keypad[num][0] - dist_r[0]) ** 2 + abs(keypad[num][1] - dist_r[1]) ** 2)
-            if math.floor(d_l) == d_l:
-                d_l = d_l
-            else:
-                d_l = math.floor(d_l) + 1
-
-            if math.floor(d_r) == d_r:
-                d_r = d_r
-            else:
-                d_r = math.floor(d_r) + 1
+            d_l = abs(keypad[num][0] - lhand[0]) + abs(keypad[num][1] - lhand[1])
+            d_r = abs(keypad[num][0] - rhand[0]) + abs(keypad[num][1] - rhand[1])
 
             if d_l > d_r:
                 answer += 'R'
-                dist_r = keypad[num]
+                rhand = keypad[num]
             elif d_l < d_r:
                 answer += 'L'
-                dist_l = keypad[num]
+                lhand = keypad[num]
             elif d_l == d_r:
                 if hand == "right":
                     answer += 'R'
-                    dist_r = keypad[num]
+                    rhand = keypad[num]
                 elif hand == "left":
                     answer += 'L'
-                    dist_l = keypad[num]
+                    lhand = keypad[num]
 
     return answer
 
