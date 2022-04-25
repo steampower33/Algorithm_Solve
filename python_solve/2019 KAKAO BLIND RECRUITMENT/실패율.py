9aba738a0d24da02f74bb5d43670bc512bf95518
steampower33 <steampower33@naver.com>
2022-04-25 21:00:15 +0900

    2019 카카오 블라인드 실패율 solve:python3

---
+++ python_solve/2019 KAKAO BLIND RECRUITMENT/실패율.py
@@ -0,0 +1,21 @@
+def solution(N, stages):
+    l = len(stages)
+    failure = dict()
+    last_cnt = 0
+    a = list()
+
+    for _ in range(1, N+1):
+        if (l - last_cnt) != 0:
+            failure[_] = stages.count(_) / (l - last_cnt)
+        elif (l - last_cnt) == 0:
+            failure[_] = 0
+        l -= last_cnt
+        last_cnt = stages.count(_)
+
+    for _ in failure.items(): a.append(_)
+
+    a.sort(key=lambda x: x[1], reverse=True)
+
+    return [_[0] for _ in a]
+
+print(solution(3, [1,1,1]))
