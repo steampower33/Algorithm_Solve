ed181cb8f7c32fd4cccea5e88485a8a8399bebe0
steampower33 <steampower33@naver.com>
2023-03-08 15:14:39 +0900

    baekjoon 11382

---
+++ c++_solve/입출력과 사칙연산/11382/11382.cpp
@@ -0,0 +1,12 @@
+// 11382 꼬마 정민
+#include <iostream>
+
+int main(void)
+{
+    long a, b, c;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
+    std::cout << a + b + c << std::endl;
+    return 0;
+}
